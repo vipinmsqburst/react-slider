27786173992d3aa9c31e788226c5fd0f3af1e952
Vipin M S <vipinm@qburst.com>
2019-12-23 19:10:14 +0530

    Initial commit

---
+++ src/slide/slide.jsx
@@ -0,0 +1,22 @@
+import React, { Component} from "react";
+import '../App.css';
+// import ReactDOM from 'react-dom';
+
+class Slide extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <div className="card" >
+            <div className="header">
+                <img className="rounded-circle" alt={this.props.slide.title} src={this.props.slide.image}/>
+            </div>
+            <div className="desc text-center">
+                <h4>{this.props.slide.title}</h4>
+                <p>{this.props.slide.description}</p>
+            </div>
+        </div>
+    }
+}
+
+export default Slide;
